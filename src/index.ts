272fe59fae162cac822c92aac872fe1b7f9e5183
furkle <furkle@furkleindustries.com>
2019-01-03 02:35:31 -0500

    Fixing build size issue.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 export * from './Manager/Manager';
 export * from './Manager/IManager';
 export * from './Manager/IManagerOptions';
-export * from './Manager/IManagerOptions';
 
 export * from './Group/Group';
 export * from './Group/IGroup';
 export * from './Group/IGroupOptions';
-export * from './Group/ISoundsMap';
 
 export * from './Sound/Sound';
 export * from './Sound/ISound';
