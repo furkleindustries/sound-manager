815e14fad5098d72470af7359fc044c760b7b318
furkle <furkle@furkleindustries.com>
2019-01-05 00:39:23 -0500

    Fixing typo.

--- src/Sound/trySetSoundLoop.ts
+++ src/Sound/trySetSoundLoop.ts
@@ -3,7 +3,7 @@ import {
 } from './ISound';
 
 export function trySetSoundLoop(sound: ISound, value?: boolean) {
-  if (typeof value === 'function') {
+  if (typeof value === 'boolean') {
     sound.setLoop(value);
   }
 }
