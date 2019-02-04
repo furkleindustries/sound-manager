fe1a7a4465c81b969e113f7ce0f7e44d8715d3de
furkle <furkle@furkleindustries.com>
2019-02-03 23:11:11 -0500

    Expanding index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
+export * from './Manager/createManager';
 export * from './Manager/Manager';
 export * from './Manager/IManager';
 export * from './Manager/IManagerOptions';
 
+export * from './Group/createGroup';
 export * from './Group/Group';
 export * from './Group/IGroup';
 export * from './Group/IGroupOptions';
 export * from './Group/createGroup';
 
+export * from './Sound/createSound';
 export * from './Sound/Sound';
 export * from './Sound/ISound';
 export * from './Sound/ISoundOptions';
 export * from './Sound/createSound';
+
+export * from './Playlist/createPlaylist';
+export * from './Playlist/Playlist';
+export * from './Playlist/IPlaylist';
+export * from './Playlist/IPlaylistOptions';
+
+export * from './AnalysisSuite/AnalysisSuite';
+export * from './AnalysisSuite/IAnalysisSuite';
+export * from './AnalysisSuite/Analysis';
+export * from './AnalysisSuite/IAnalysis';
+export * from './AnalysisSuite/IAnalysisOptions';
+export * from './AnalysisSuite/IAnalysisRenderCallback';
+export * from './AnalysisSuite/IGetAnalysisOptions';
 
 export * from './functions/loadAudioBuffer';
